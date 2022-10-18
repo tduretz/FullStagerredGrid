fdb14488313d055675c4cf8b5cfd373fbe5e0470
Duretz Thibault <thibault.duretz@univ-rennes1.fr>
2022-10-18 21:14:54 +0200

    towards free surface

--- scripts/Stokes2D_FSG_PT_v2.jl
+++ scripts/Stokes2D_FSG_PT_v2.jl
@@ -21,26 +21,23 @@ function PatchPlotMakie(vertx, verty, sol, xmin, xmax, ymin, ymax, x1, y1, x2, y
     min_v = minimum( sol.p ); max_v = maximum( sol.p )
     limits = min_v ≈ max_v ? (min_v, min_v + 1) : (min_v, max_v)
     p = [Polygon( Point2f0[ (vertx[i,j], verty[i,j]) for j=1:4] ) for i in 1:length(sol.p)]
-    CairoMakie.poly!(p, color = sol.p, colormap = cmap, strokewidth = 1, strokecolor = :white, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect=:image, colorrange=limits)
+    CairoMakie.poly!(p, color = sol.p, colormap = cmap, strokewidth = 0, strokecolor = :white, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect=:image, colorrange=limits)
 
     CairoMakie.Axis(f[2,1], aspect = ar)
     min_v = minimum( sol.vx ); max_v = maximum( sol.vx )
     limits = min_v ≈ max_v ? (min_v, min_v + 1) : (min_v, max_v)
     p = [Polygon( Point2f0[ (vertx[i,j], verty[i,j]) for j=1:4] ) for i in 1:length(sol.vx)]
-    CairoMakie.poly!(p, color = sol.vx, colormap = cmap, strokewidth = 1, strokecolor = :white, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect=:image, colorrange=limits)
+    CairoMakie.poly!(p, color = sol.vx, colormap = cmap, strokewidth = 0, strokecolor = :white, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect=:image, colorrange=limits)
 
     CairoMakie.Axis(f[2,2], aspect = ar)
     min_v = minimum( sol.vy ); max_v = maximum( sol.vy )
     limits = min_v ≈ max_v ? (min_v, min_v + 1) : (min_v, max_v)
     p = [Polygon( Point2f0[ (vertx[i,j], verty[i,j]) for j=1:4] ) for i in 1:length(sol.vx)]
-    CairoMakie.poly!(p, color = sol.vy, colormap = cmap, strokewidth = 1, strokecolor = :white, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect=:image, colorrange=limits)
+    CairoMakie.poly!(p, color = sol.vy, colormap = cmap, strokewidth = 0, strokecolor = :white, markerstrokewidth = 0, markerstrokecolor = (0, 0, 0, 0), aspect=:image, colorrange=limits)
 
-
-      # CairoMakie.scatter!(x1,y1, color=:white)
+    # CairoMakie.scatter!(x1,y1, color=:white)
     # CairoMakie.scatter!(x2,y2, color=:white, marker=:xcross)
-    CairoMakie.Colorbar(f, colormap = cmap, limits=limits, flipaxis = true, size = 25 )
-
-    
+    # CairoMakie.Colorbar(f, colormap = cmap, limits=limits, flipaxis = true, size = 25 )    
     display(f)
     return nothing
 end
