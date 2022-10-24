f48a625062345eaf2f094dbc15495efc9c5f4613
tduretz <imac@geo145.geophysik.uni-frankfurt.de>
2022-10-24 12:12:13 +0200

    v5 has both x and y deformation

--- scripts/Stokes2D_FSG_PT_v5.jl
+++ scripts/Stokes2D_FSG_PT_v5.jl
@@ -1,8 +1,9 @@
-# TRY EXPONENTIAL MESH
+# TRY EXPONENTIAL MESH IN BOTH X AND Y
 # Initialisation
 using Plots, Printf, LinearAlgebra, SpecialFunctions
 import CairoMakie
 using Makie.GeometryBasics, FileIO, ForwardDiff
+include("MeshDeformation.jl")
 # Macros
 @views    ∂_∂x(f1,f2,Δx,Δy,∂ξ∂x,∂η∂x) = ∂ξ∂x.*(f1[2:size(f1,1),:] .- f1[1:size(f1,1)-1,:]) ./ Δx .+ ∂η∂x.*(f2[:,2:size(f2,2)] .- f2[:,1:size(f2,2)-1]) ./ Δy
 @views    ∂_∂y(f1,f2,Δx,Δy,∂ξ∂y,∂η∂y) = ∂ξ∂y.*(f2[2:size(f2,1),:] .- f2[1:size(f2,1)-1,:]) ./ Δx .+ ∂η∂y.*(f1[:,2:size(f1,2)] .- f1[:,1:size(f1,2)-1]) ./ Δy
@@ -62,7 +63,11 @@ function Mesh_y( X, A, x0, σ, b, m, ymin0, ymax0, σy )
     return y
 end
 function Mesh_x( X, A, x0, σ, b, m, xmin0, xmax0, σx )
-    x   = X[1]
+    xmin1 = (sinh.( σx.*(xmin0.-x0) ))
+    xmax1 = (sinh.( σx.*(xmax0.-x0) ))
+    sx    = (xmax0-xmin0)/(xmax1-xmin1)
+    x     = (sinh.( σx.*(X[1].-x0) )) .* sx  .+ x0        
+    # x   = X[1]
     return x
 end
 
@@ -85,8 +90,8 @@ end
     nout     = 1000      # residual check frequency
     # Iterative parameters -------------------------------------------
     if adapt_mesh
-        Reopt    = 0.5*π*1.75
-        cfl      = 3.1
+        Reopt    = 0.5*π
+        cfl      = 2.0
         nsm      = 4
     else
         Reopt    = 0.625*π*2
@@ -145,176 +150,36 @@ end
     ∂η∂y =  ones(2ncx+3, 2ncy+3)
     hx   = zeros(2ncx+3, 2ncy+3)
     if adapt_mesh
-
         x0     = (xmax + xmin)/2
         m      = ymin
         Amp    = 1.0
         σ      = 0.5
         σx     = 0.5
         σy     = 0.5
-        b      = ymax
         ϵ      = 1e-7
-
         # copy initial y
-        y_ini  = copy(yv4)
         x_ini  = copy(xv4)
+        y_ini  = copy(yv4)
         X_msh  = zeros(2)
-
-        X, Y   = x_ini, y_ini
-        z0     =    -h.(X, Amp, σ, ymax, x0) # negative to conform with De La Puente paper
-        hx     = -dhdx.(X, Amp, σ, ymax, x0)
-        ∂η∂x  .= (ymin .- Y)./(z0.+m) .* hx  # detadx
-        ∂η∂y  .= ymin./(z0.+m)               # detady
-        for i in eachindex(X)          
+        # Compute slope
+        hx     = -dhdx.(x_ini, Amp, σ, ymax, x0)
+        # Deform mesh
+        for i in eachindex(x_ini)          
             X_msh[1] = x_ini[i]
             X_msh[2] = y_ini[i]     
+            xv4[i]   =  Mesh_x( X_msh,  Amp, x0, σ, ymax, m, xmin, xmax, σx )
             yv4[i]   =  Mesh_y( X_msh,  Amp, x0, σ, ymax, m, ymin, ymax, σy )
         end
-
-        ∂x∂ξ = zeros(size(yv4))
-        ∂x∂η = zeros(size(yv4))
-        ∂y∂ξ = zeros(size(yv4))
-        ∂y∂η = zeros(size(yv4))
-        @time for i in eachindex(X)          
-
-            # compute dxdksi
-            X_msh[1] = x_ini[i]-ϵ
-            X_msh[2] = y_ini[i] 
-            xm       = Mesh_x( X_msh,  Amp, x0, σ, xmax, m, xmin, xmax, σx )
-            # --------
-            X_msh[1] = x_ini[i]+ϵ
-            X_msh[2] = y_ini[i]
-            xp       = Mesh_x( X_msh,  Amp, x0, σ, xmax, m, xmin, xmax, σx )
-            # --------
-            ∂x∂ξ[i] = (xp - xm) / (2ϵ)
-
-            # compute dydeta
-            X_msh[1] = x_ini[i]
-            X_msh[2] = y_ini[i]-ϵ
-            xm     = Mesh_x( X_msh,  Amp, x0, σ, ymax, m, ymin, ymax, σy )
-            # --------
-            X_msh[1] = x_ini[i]
-            X_msh[2] = y_ini[i]+ϵ
-            xp       = Mesh_x( X_msh,  Amp, x0, σ, ymax, m, ymin, ymax, σy )
-            # --------
-            ∂x∂η[i] = (xp - xm) / (2ϵ)
-
-            # compute dydksi
-            X_msh[1] = x_ini[i]-ϵ
-            X_msh[2] = y_ini[i] 
-            ym       = Mesh_y( X_msh,  Amp, x0, σ, ymax, m, ymin, ymax, σy )
-            # --------
-            X_msh[1] = x_ini[i]+ϵ
-            X_msh[2] = y_ini[i]
-            yp       = Mesh_y( X_msh,  Amp, x0, σ, ymax, m, ymin, ymax, σy )
-            # --------
-            ∂y∂ξ[i] = (yp - ym) / (2ϵ)
-
-            # compute dydeta
-            X_msh[1] = x_ini[i]
-            X_msh[2] = y_ini[i]-ϵ
-            ym     = Mesh_y( X_msh,  Amp, x0, σ, ymax, m, ymin, ymax, σy )
-            # --------
-            X_msh[1] = x_ini[i]
-            X_msh[2] = y_ini[i]+ϵ
-            yp     = Mesh_y( X_msh,  Amp, x0, σ, ymax, m, ymin, ymax, σy )
-            # --------
-            ∂y∂η[i] = (yp - ym) / (2ϵ)
-        end
-        # #################
-        # # ForwardDiff
-        # g = zeros(2)
-        # Y = zeros(1)
-        # dydksi_FD = zeros(size(dydeta))
-        # dydeta_FD = zeros(size(dydeta))
-        # dxdksi_FD = zeros(size(dydeta))
-        # dxdeta_FD = zeros(size(dydeta))
-        # @time for i in eachindex(dydeta_FD)
-        #     X_msh[1] = x_ini[i]
-        #     X_msh[2] = y_ini[i]
-        #     Mesh_y_closed = (X_msh) -> Mesh_y( X_msh, Amp, x0, σ, b, m, ymin )
-        #     ForwardDiff.gradient!( g, Mesh_y_closed, X_msh )
-        #     dydksi_FD[i] = g[1]
-        #     dydeta_FD[i] = g[2]
-        #     Mesh_x_closed = (X_msh) -> Mesh_x( X_msh, Amp, x0, σ, b, m, ymin )
-        #     ForwardDiff.gradient!( g, Mesh_x_closed, X_msh )
-        #     dxdksi_FD[i] = g[1]
-        #     dxdeta_FD[i] = g[2]
-        # end
-
-        # dxdksi_num = diff(xv4,dims=1)/(Δx/2)
-        # dxdeta_num = diff(xv4,dims=2)/(Δy/2)
-        # dydksi_num = diff(yv4,dims=1)/(Δx/2)
-        # dydeta_num = diff(yv4,dims=2)/(Δy/2)
-
-        # @printf("min(dxdksi    ) = %1.6f --- max(dxdksi    ) = %1.6f\n", minimum(dxdksi   ), maximum(dxdksi   ))
-        # @printf("min(dxdksi_FD ) = %1.6f --- max(dxdksi_FD ) = %1.6f\n", minimum(dxdksi_FD), maximum(dxdksi_FD))
-        # @printf("min(dxdksi_num) = %1.6f --- max(dxdksi_num) = %1.6f\n", minimum(dxdksi_num), maximum(dxdksi_num))
-
-        # @printf("min(dxdeta    ) = %1.6f --- max(dxdeta   ) = %1.6f\n", minimum(dxdeta   ), maximum(dxdeta   ))
-        # @printf("min(dxdeta_FD ) = %1.6f --- max(dxdeta_FD) = %1.6f\n", minimum(dxdeta_FD), maximum(dxdeta_FD))
-        # @printf("min(dxdeta_num) = %1.6f --- max(dxdeta_num) = %1.6f\n", minimum(dxdeta_num), maximum(dxdeta_num))
-
-        # @printf("min(dydksi    ) = %1.6f --- max(dydksi    ) = %1.6f\n", minimum(dydksi   ), maximum(dydksi   ))
-        # @printf("min(dydksi_FD ) = %1.6f --- max(dydksi_FD ) = %1.6f\n", minimum(dydksi_FD), maximum(dydksi_FD))
-        # @printf("min(dydksi_num) = %1.6f --- max(dydksi_num) = %1.6f\n", minimum(dydksi_num), maximum(dydksi_num))
-
-        # @printf("min(dydeta    ) = %1.6f --- max(dydeta    ) = %1.6f\n", minimum(dydeta   ), maximum(dydeta   ))
-        # @printf("min(dydeta_FD ) = %1.6f --- max(dydeta_FD ) = %1.6f\n", minimum(dydeta_FD), maximum(dydeta_FD))
-        # @printf("min(dydeta_num) = %1.6f --- max(dydeta_num) = %1.6f\n", minimum(dydeta_num), maximum(dydeta_num))
-
-        # #################
-
-        M = zeros(2,2)
-        @time for i in eachindex(yv4)
-            M[1,1]  = ∂x∂ξ[i]
-            M[1,2]  = ∂x∂η[i]
-            M[2,1]  = ∂y∂ξ[i]
-            M[2,2]  = ∂y∂η[i]
-            invJ    = inv(M)
-            ∂ξ∂x[i] = invJ[1,1]
-            ∂ξ∂y[i] = invJ[1,2]
-            ∂η∂x[i] = invJ[2,1]
-            ∂η∂y[i] = invJ[2,2]
-        end
-
-        @printf("min(∂ξ∂x) = %1.6f --- max(∂ξ∂x) = %1.6f\n", minimum(∂ξ∂x), maximum(∂ξ∂x))
-
-        @printf("min(∂ξ∂y) = %1.6f --- max(∂ξ∂y) = %1.6f\n", minimum(∂ξ∂y), maximum(∂ξ∂y))
-
-        @printf("min(∂η∂x) = %1.6f --- max(∂η∂x) = %1.6f\n", minimum(∂η∂x), maximum(∂η∂x))
-
-        @printf("min(∂η∂y) = %1.6f --- max(∂η∂y) = %1.6f\n", minimum(∂η∂y), maximum(∂η∂y))
-
-
-        # # First shift x
-        # x0       = (xmax + xmin)/2
-        # σx       = 0.5
-
-        # xmin0    = minimum(xv4)
-        # xmax0    = maximum(xv4)
-        # xmin1    = (sinh.( σx.*(xmin0.-x0) ))
-        # xmax1    = (sinh.( σx.*(xmax0.-x0) ))
-        # sx       = (xmax0-xmin0)/(xmax1-xmin1)
-        # xv4      = (sinh.( σx.*(xv4.-x0) )) .* sx  .+ x0
-        # @printf("Check width = %2.2e\n", (maximum(xv4)-minimum(xv4)) - (xmax0-xmin0))
-        
-        # y0       = ymax
-        # σy       = 0.5
-        
-        # ymin0    = minimum(yv4)
-        # ymax0    = maximum(yv4)
-        # ymin1    = (sinh.( σy.*(ymin0.-y0) ))
-        # ymax1    = (sinh.( σy.*(ymax0.-y0) ))
-        # sy       = (ymax0-ymin0)/(ymax1-ymin1)
-        # yv4      = (sinh.( σy.*(yv4.-y0) )) .* sy  .+ y0
-        # @printf("Check height = %2.2e\n", (maximum(yv4)-minimum(yv4)) - (ymax0-ymin0))
-        
-        # # then evaluate topo = f(x)
-        # z0       = -h.(xv4, 1., 0.5, maximum(yv4), x0) # negative to conform with De La Puente paper
-        # m        = minimum(yv4)
-        # yv4      = (yv4./m).*((z0.+m)).-z0 
-
+        # Compute forward transformation
+        params = (Amp=Amp, x0=x0, σ=σ, m=m, xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, σx=σx, σy=σy, ϵ=ϵ)
+        ∂x     = (∂ξ=zeros(size(yv4)), ∂η = zeros(size(yv4)) )
+        ∂y     = (∂ξ=zeros(size(yv4)), ∂η = zeros(size(yv4)) )
+        ComputeForwardTransformation!( ∂x, ∂y, x_ini, y_ini, X_msh, Amp, x0, σ, m, xmin, xmax, ymin, ymax, σx, σy, ϵ)
+        # Solve for inverse transformation
+        ∂ξ = (∂x=∂ξ∂x, ∂y=∂ξ∂y); ∂η = (∂x=∂η∂x, ∂y=∂η∂y)
+        InverseJacobian!(∂ξ,∂η,∂x,∂y)
+        ∂ξ∂x .= ∂ξ.∂x; ∂ξ∂y .= ∂ξ.∂y
+        ∂η∂x .= ∂η.∂x; ∂η∂y .= ∂η.∂y
     end
     # Grid subsets
     xv2_1, yv2_1 = xv4[2:2:end-1,2:2:end-1  ], yv4[2:2:end-1,2:2:end-1  ]
@@ -369,36 +234,32 @@ end
     κΔτp_2 .= cfl .* η_2[:,1:end-1] .* Δx ./ (xmax-xmin)
     if solve
         hx_surf =   hx[3:2:end-2, end]
-        C_surf  = ∂η∂y[3:2:end-2, end]
         η_surf  = η_1[:,end]
-        M1   =  0.5*hx_surf.^4 .+ 1.0*hx_surf.^2 .+ 0.5
-        M1x2 =  1.0*hx_surf.^4 .+ 2.0*hx_surf.^2 .+ 1.0
-        M2   = 0.25*hx_surf.^2 .+ 0.5
-        M3   =  0.5*hx_surf.^2 .+ 0.75
-        M4   =      hx_surf.^2 .+ 1
-        M5   =  0.5*hx_surf.^2 .+ 0.25
+        dx      = Δx
+        dz      = Δy
+        dkdx    = ∂ξ∂x[3:2:end-2, end]
+        dkdy    = ∂ξ∂y[3:2:end-2, end]
+        dedx    = ∂η∂x[3:2:end-2, end]
+        dedy    = ∂η∂y[3:2:end-2, end]
+
+        M1 = dedx.*hx_surf.^2 .- dedx .+ 2*dedy.*hx_surf
+        M2 = 2*dedx.^2 .*hx_surf.^2 .+ dedx.^2 .+ 2*dedx.*dedy.*hx_surf .+ dedy.^2 .*hx_surf.^2 .+ 2*dedy.^2
+        M3 = 2*dedx.*hx_surf.^2 .+ dedx .+ 2*dedy.*hx_surf
+        M4 = hx_surf.^2 .+ 2
+        M5 = 2*hx_surf.^2 .+ 1
+        M6 = 2*dedx.*hx_surf - dedy.*hx_surf.^2 .+ dedy
         # PT loop
         it=1; iter=1; err=2*ε; err_evo1=[]; err_evo2=[];
         while (err>ε && iter<=iterMax)
-
             # !!!!!!!!! Change derivatives !!!!!
             dVxdx  = (Vx_1[2:end-0,end] - Vx_1[1:end-1,end])/Δx
             dVydx  = (Vy_1[2:end-0,end] - Vy_1[1:end-1,end])/Δx
-            @show size(dVxdx)
-            @show size(dVydx)
-            dVxdx  = ∂_∂x(Vx_1[:,end],Vx_2[2:end-1,end],Δx,Δy,∂ξ∂xc_1[:,end],∂η∂xc_1[:,end])
-            dVydx  = ∂_∂y(Vy_2[2:end-1,end],Vy_1[:,end],Δx,Δy,∂ξ∂yc_1[:,end],∂η∂yc_1[:,end])
-            @show size(dVxdx)
-            @show size(dVydx)
-            error()
             P_surf = P_1[:,end]
-
-            # See python notebook v4
-            Vx_2[2:end-1,end] = (2.0*C_surf.*M1.*η_surf.*Vx_2[2:end-1,end-1] - 2.0*M2.*dVydx.*Δy.*η_surf - 2.0*M3.*dVxdx.*Δy.*η_surf.*hx_surf + 0.75*M4.*P_surf.*Δy.*hx_surf)./(C_surf.*M1x2.*η_surf)  
-            Vy_2[2:end-1,end] = (2.0*C_surf.*M1.*η_surf.*Vy_2[2:end-1,end-1] + 2.0*M5.*dVxdx.*Δy.*η_surf - 2.0*M5.*dVydx.*Δy.*η_surf.*hx_surf + 0.75*M4.*P_surf.*Δy)./(C_surf.*M1x2.*η_surf)  
-            # See python notebook v3
-            # Vx_2[2:end-1,end] = Vx_2[2:end-1,end-1] - Δy*1/1*dVydx # dvxdy = -(dvydx)
-            # Vy_2[2:end-1,end] = Vy_2[2:end-1,end-1] + Δy*1/2*dVxdx + 3/4 * Δy*P_surf./η_surf # dvydy = 1/2*dv(dvxdx) + 3/4 * P/eta
+            # See python notebook v5
+            Vx_2[2:end-1,end] = (3*M1.*P_surf.*dz/2 + M2.*η_surf.*Vx_2[2:end-1,end-1] - M3.*dkdx.*dVxdx.*dz.*η_surf - M4.*dedy.*dkdx.*dVydx.*dz.*η_surf + dedx.*dkdy.*dz.*η_surf.*hx_surf.^2 + 2*dedx.*dkdy.*dz.*η_surf - dedy.*dkdy.*dz.*η_surf.*hx_surf.^2 - 2*dedy.*dkdy.*dz.*η_surf)./(M2.*η_surf)
+            Vy_2[2:end-1,end] = (M2.*η_surf.*Vy_2[2:end-1,end-1] - M5.*dedx.*dkdx.*dVydx.*dz.*η_surf + M5.*dedy.*dkdx.*dVxdx.*dz.*η_surf + 3*M6.*P_surf.*dz/2 - 2*dedx.*dkdy.*dz.*η_surf.*hx_surf.^2 - 2*dedx.*dkdy.*dz.*η_surf.*hx_surf - dedx.*dkdy.*dz.*η_surf - dedy.*dkdy.*dz.*η_surf.*hx_surf.^2 - 2*dedy.*dkdy.*dz.*η_surf)./(M2.*η_surf)
+            # Vx_2[2:end-1,end] = (2.0*C_surf.*M1.*η_surf.*Vx_2[2:end-1,end-1] - 2.0*M2.*dVydx.*Δy.*η_surf - 2.0*M3.*dVxdx.*Δy.*η_surf.*hx_surf + 0.75*M4.*P_surf.*Δy.*hx_surf)./(C_surf.*M1x2.*η_surf)  
+            # Vy_2[2:end-1,end] = (2.0*C_surf.*M1.*η_surf.*Vy_2[2:end-1,end-1] + 2.0*M5.*dVxdx.*Δy.*η_surf - 2.0*M5.*dVydx.*Δy.*η_surf.*hx_surf + 0.75*M4.*P_surf.*Δy)./(C_surf.*M1x2.*η_surf)  
             ∇v_1            .=  ∂_∂x(Vx_1,Vx_2[2:end-1,:],Δx,Δy,∂ξ∂xc_1,∂η∂xc_1) .+ ∂_∂y(Vy_2[2:end-1,:],Vy_1,Δx,Δy,∂ξ∂yc_1,∂η∂yc_1) 
             ∇v_2[:,1:end-1] .=  ∂_∂x(Vx_2[:,2:end-1],Vx_1,Δx,Δy,∂ξ∂xc_2,∂η∂xc_2) .+ ∂_∂y(Vy_1,Vy_2[:,2:end-1],Δx,Δy,∂ξ∂yc_2,∂η∂yc_2) 
             ε̇xx_1 .=  ∂_∂x(Vx_1,Vx_2[2:end-1,:],Δx,Δy,∂ξ∂xc_1,∂η∂xc_1) .- 1.0/3.0*∇v_1
@@ -413,7 +274,6 @@ end
             τyy_2 .= 2.0 .* η_2 .* ε̇yy_2
             τxy_1 .= 2.0 .* η_1 .* ε̇xy_1
             τxy_2 .= 2.0 .* η_2 .* ε̇xy_2
-            # if iter==1000 @show τxx_1[:,end] enξ
             Rx_1[2:end-1,2:end-0] .= ∂_∂x(τxx_1[:,2:end-0],τxx_2[2:end-1,:],Δx,Δy,∂ξ∂xv_1[2:end-1,2:end-0],∂η∂xv_1[2:end-1,2:end-0]) .+ ∂_∂y(τxy_2[2:end-1,:],τxy_1[:,2:end-0],Δx,Δy,∂ξ∂yv_1[2:end-1,2:end-0],∂η∂yv_1[2:end-1,2:end-0]) .-  ∂_∂x(P_1[:,2:end-0],P_2[2:end-1,:],Δx,Δy,∂ξ∂xv_1[2:end-1,2:end-0],∂η∂xv_1[2:end-1,2:end-0])
             Rx_2                  .= ∂_∂x(τxx_2[:,1:end-1],τxx_1,           Δx,Δy,∂ξ∂xv_2[2:end-1,2:end-1],∂η∂xv_2[2:end-1,2:end-1]) .+ ∂_∂y(τxy_1,τxy_2[:,1:end-1],           Δx,Δy,∂ξ∂yv_2[2:end-1,2:end-1],∂η∂yv_2[2:end-1,2:end-1]) .-  ∂_∂x(P_2[:,1:end-1],P_1,           Δx,Δy,∂ξ∂xv_2[2:end-1,2:end-1],∂η∂xv_2[2:end-1,2:end-1]) 
             Ry_1[2:end-1,2:end-0] .= ∂_∂y(τyy_2[2:end-1,:],τyy_1[:,2:end-0],Δx,Δy,∂ξ∂yv_1[2:end-1,2:end-0],∂η∂yv_1[2:end-1,2:end-0]) .+ ∂_∂x(τxy_1[:,2:end-0],τxy_2[2:end-1,:],Δx,Δy,∂ξ∂xv_1[2:end-1,2:end-0],∂η∂xv_1[2:end-1,2:end-0]) .-  ∂_∂y(P_2[2:end-1,:],P_1[:,2:end-0],Δx,Δy,∂ξ∂yv_1[2:end-1,2:end-0],∂η∂yv_1[2:end-1,2:end-0]) .+ ρ_1[2:end-1,2:end-0].*g
