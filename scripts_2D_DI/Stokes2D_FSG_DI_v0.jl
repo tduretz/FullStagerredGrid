ad1693c7f048cfc2c9a0c3dc006f5f7fba367006
tduretz <imac@geo145.geophysik.uni-frankfurt.de>
2022-11-04 14:44:15 +0100

    add residual evaluation

--- scripts_2D_DI/Stokes2D_FSG_DI_v0.jl
+++ scripts_2D_DI/Stokes2D_FSG_DI_v0.jl
@@ -2,98 +2,162 @@ using Plots, Printf, LinearAlgebra, SpecialFunctions
 # Macros
 @views    ∂_∂x(f1,f2,Δx,Δy,∂ξ∂x,∂η∂x) = ∂ξ∂x.*(f1[2:size(f1,1),:] .- f1[1:size(f1,1)-1,:]) ./ Δx .+ ∂η∂x.*(f2[:,2:size(f2,2)] .- f2[:,1:size(f2,2)-1]) ./ Δy
 @views    ∂_∂y(f1,f2,Δx,Δy,∂ξ∂y,∂η∂y) = ∂ξ∂y.*(f2[2:size(f2,1),:] .- f2[1:size(f2,1)-1,:]) ./ Δx .+ ∂η∂y.*(f1[:,2:size(f1,2)] .- f1[:,1:size(f1,2)-1]) ./ Δy
+@views    ∂_∂(fE,fW,fN,fS,Δ,a,b) = a*(fE - fW) / Δ.x .+ b*(fN - fS) / Δ.y
+function Main_2D_DI()
+    # Physics
+    # x          = (min=-3.0, max=3.0)  
+    # y          = (min=-5.0, max=0.0)
+    x          = (min=-3.0, max=3.0)  
+    y          = (min=-3.0, max=3.0)
+    ε̇bg        = -1.0
+    rad        = 0.5
+    y0         = -2*0.
+    g          = (x = 0., z=-1.0*0.0)
+    inclusion  = true
+    adapt_mesh = true
+    solve      = true
+    # Numerics
+    nc         = (x=11,     y=11    )  # numerical grid resolution
+    nv         = (x=nc.x+1, y=nc.y+1)  # numerical grid resolution
+    ε          = 1e-8          # nonlinear tolerance
+    iterMax    = 20            # max number of iters
+    # Preprocessing
+    Δ          = (x=(x.max-x.min)/nc.x, y=(y.max-y.min)/nc.y)
+    # Array initialisation
+    V        = ( x   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)),  # v: vertices --- c: centroids
+                 y   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)) ) 
+    ε̇        = ( xx  = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),  # ex: x edges --- ey: y edges
+                 yy  = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 xy  = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)) )
+    τ        = ( xx  = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),  # ex: x edges --- ey: y edges
+                 yy  = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 xy  = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)) )
+    ∇v̇       = (        ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)  )
+    P        = (        ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)  )
+    D        = ( v11 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 v12 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),  
+                 v13 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 v21 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 v22 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 v23 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 v31 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 v32 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)),
+                 v33 = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)), )
+    ∂ξ       = ( ∂x  = (v  =  ones(nv.x,   nv.y), c  =  ones(nc.x+2, nc.y+2), ex =  ones(nc.x+2, nv.y), ey =  ones(nv.x,   nc.y+2)), 
+                 ∂y  = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2), ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)) )
+    ∂η       = ( ∂x  = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2), ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)), 
+                 ∂y  = (v  =  ones(nv.x,   nv.y), c  =  ones(nc.x+2, nc.y+2), ex =  ones(nc.x+2, nv.y), ey =  ones(nv.x,   nc.y+2)) ) 
+    R        = ( x   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)), 
+                 y   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)),
+                 p   = (ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)) ) 
+    ρ        = ( (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)) )
+    η        = (        ex = zeros(nc.x+2, nv.y), ey = zeros(nv.x,   nc.y+2)  )
+    BC       = (x = zeros(Int, nv.x,   nv.y), y = zeros(Int, nv.x,   nv.y) )
+    # Fine mesh
+    xxv, yyv    = LinRange(x.min-Δ.x/2, x.max+Δ.x/2, 2nc.x+3), LinRange(y.min-Δ.y/2, y.max+Δ.y/2, 2nc.y+3)
+    (xv4,yv4) = ([x for x=xxv,y=yyv], [y for x=xxv,y=yyv])
+    xv2_1, yv2_1 = xv4[2:2:end-1,2:2:end-1  ], yv4[2:2:end-1,2:2:end-1  ]
+    xv2_2, yv2_2 = xv4[1:2:end-0,1:2:end-0  ], yv4[1:2:end-0,1:2:end-0  ]
+    xc2_1, yc2_1 = xv4[3:2:end-2,2:2:end-1  ], yv4[3:2:end-2,2:2:end-1  ]
+    xc2_2, yc2_2 = xv4[2:2:end-1,3:2:end-2+2], yv4[2:2:end-1,3:2:end-2+2]
+    x = merge(x, (v=xv4[2:2:end-1,2:2:end-1  ], c=xv4[1:2:end-0,1:2:end-0  ], ex=xv4[1:2:end,2:2:end-1  ], ey=xv4[2:2:end-1,1:2:end]) ) 
+    y = merge(x, (v=yv4[2:2:end-1,2:2:end-1  ], c=yv4[1:2:end-0,1:2:end-0  ], ex=yv4[1:2:end,2:2:end-1  ], ey=yv4[2:2:end-1,1:2:end]) ) 
+    # Velocity
+    V.x.v .= -ε̇bg.*x.v; V.x.c .= -ε̇bg.*x.c
+    V.y.v .=  ε̇bg.*y.v; V.y.c .=  ε̇bg.*y.c
+    # Viscosity
+    η.ex  .= 1.0; η.ey  .= 1.0
+    if inclusion
+        η.ex[x.ex.^2 .+ (y.ex.-y0).^2 .< rad] .= 100.
+        η.ey[x.ey.^2 .+ (y.ey.-y0).^2 .< rad] .= 100.
+    end
+    D.v11.ex .= 2 .* η.ex; D.v11.ey .= 2 .* η.ey
+    D.v22.ex .= 2 .* η.ex; D.v22.ey .= 2 .* η.ey
+    D.v33.ex .= 2 .* η.ex; D.v33.ey .= 2 .* η.ey
+    # Density
+    ρ.v  .= 1.0; ρ.c  .= 1.0
+    if inclusion
+        ρ.v[x.v.^2 .+ (y.v.-y0).^2 .< rad] .= 2.
+        ρ.c[x.c.^2 .+ (y.c.-y0).^2 .< rad] .= 2.
+    end
+    # Boundary conditions
+    BCVx = (West=:Dirichlet, East=:Dirichlet, South=:Dirichlet, North=:Dirichlet)
+    BCVy = (West=:Dirichlet, East=:Dirichlet, South=:Dirichlet, North=:Dirichlet)
+    # Vx
+    if BCVx.West == :Dirichlet 
+        BC.x[1,:]   .= 1
+    end
+    if BCVx.East == :Dirichlet 
+        BC.x[end,:] .= 1
+    end
+    if BCVx.South == :Dirichlet 
+        BC.x[:,1]   .= 1
+    end
+    if BCVx.North == :Dirichlet 
+        BC.x[:,end] .= 1
+    end
+    # Vy
+    if BCVy.West == :Dirichlet 
+        BC.y[1,:]   .= 1
+    end
+    if BCVy.East == :Dirichlet 
+        BC.y[end,:] .= 1
+    end
+    if BCVy.South == :Dirichlet 
+        BC.y[:,1]   .= 1
+    end
+    if BCVy.North == :Dirichlet 
+        BC.y[:,end] .= 1
+    end
+    # Loop on vertices
+    @time for j in axes(R.x.v, 2), i in axes(R.x.v, 1)
+        # Stencil
+        τxxW = τ.xx.ex[i  ,j]; τyyW = τ.yy.ex[i  ,j]; τxyW = τ.xy.ex[i  ,j]; PW   = P.ex[i,  j]
+        τxxE = τ.xx.ex[i+1,j]; τyyE = τ.yy.ex[i+1,j]; τxyE = τ.xy.ex[i+1,j]; PE   = P.ex[i+1,j]
+        τxxS = τ.xx.ey[i,j  ]; τyyS = τ.yy.ey[i,j  ]; τxyS = τ.xy.ey[i,j  ]; PS   = P.ey[i,j]
+        τxxN = τ.xx.ey[i,j+1]; τyyN = τ.yy.ey[i,j+1]; τxyN = τ.xy.ey[i,j+1]; PN   = P.ey[i,j+1]   
+        ∂ξ∂x = ∂ξ.∂x.v[i,j];   ∂ξ∂y = ∂ξ.∂y.v[i,j];   ∂η∂x = ∂η.∂x.v[i,j];   ∂η∂y = ∂η.∂y.v[i,j]
+        # x
+        if BC.x[i,j] == 1
+            R.x.v[i,j] = 0.
+        else
+            R.x.v[i,j] = - (∂_∂(τxxE,τxxW,τxxN,τxxS,Δ,∂ξ∂x,∂η∂x) + ∂_∂(τxyE,τxyW,τxyN,τxyS,Δ,∂ξ∂y,∂η∂y) - ∂_∂(PE,PW,PN,PS,Δ,∂ξ∂x,∂η∂x) + ρ.v[i,j]*g.x )
+        end
+        # y
+        if BC.x[i,j] == 1
+            R.x.v[i,j] = 0.
+        else
+            R.y.v[i,j] = - (∂_∂(τyyE,τyyW,τyyN,τyyS,Δ,∂ξ∂y,∂η∂y) + ∂_∂(τxyE,τxyW,τxyN,τxyS,Δ,∂ξ∂x,∂η∂x) - ∂_∂(PE,PW,PN,PS,Δ,∂ξ∂y,∂η∂y) + ρ.v[i,j]*g.z) 
+        end
+    end 
+    # Loop on centroids
+    @time for j in axes(R.x.c, 2), i in axes(R.x.c, 1)
+        if i>1 && i<size(R.x.c,1) && j>1 && j<size(R.x.c,2)
+        # Stencil
+        τxxW = τ.xx.ey[i-1,j]; τyyW = τ.yy.ey[i-1,j]; τxyW = τ.xy.ey[i-1,j]; PW   = P.ey[i-1,j]
+        τxxE = τ.xx.ey[i,j  ]; τyyE = τ.yy.ey[i,j  ]; τxyE = τ.xy.ey[i,j  ]; PE   = P.ey[i,j  ]
+        τxxS = τ.xx.ex[i,j-1]; τyyS = τ.yy.ex[i,j-1]; τxyS = τ.xy.ex[i,j-1]; PS   = P.ex[i,j-1]
+        τxxN = τ.xx.ex[i,j  ]; τyyN = τ.yy.ex[i,j  ]; τxyN = τ.xy.ex[i,j  ]; PN   = P.ex[i,j  ]
+        ∂ξ∂x = ∂ξ.∂x.v[i,j];   ∂ξ∂y = ∂ξ.∂y.v[i,j];   ∂η∂x = ∂η.∂x.c[i,j];   ∂η∂y = ∂η.∂y.c[i,j]
+        # x
+        R.x.c[i,j] = - (∂_∂(τxxE,τxxW,τxxN,τxxS,Δ,∂ξ∂x,∂η∂x) + ∂_∂(τxyE,τxyW,τxyN,τxyS,Δ,∂ξ∂y,∂η∂y) - ∂_∂(PE,PW,PN,PS,Δ,∂ξ∂x,∂η∂x) + ρ.v[i,j]*g.x)
+        # y
+        R.y.c[i,j] = - (∂_∂(τyyE,τyyW,τyyN,τyyS,Δ,∂ξ∂y,∂η∂y) + ∂_∂(τxyE,τxyW,τxyN,τxyS,Δ,∂ξ∂x,∂η∂x) - ∂_∂(PE,PW,PN,PS,Δ,∂ξ∂y,∂η∂y) + ρ.v[i,j]*g.z) 
+        end
+    end 
+    # Loop on horizontal edges
+    @time for j in axes(R.p.ex, 2), i in axes(R.p.ex, 1)
+        if i>1 && i<size(R.p.ex, 1)
+            R.p.ex[i,j] = -∇v̇.ex[i,j]
+        end
+    end
+    # Loop on horizontal edges
+    @time for j in axes(R.p.ey, 2), i in axes(R.p.ey, 1)
+        if j>1 && j<size(R.p.ey, 2)
+            R.p.ey[i,j] = -∇v̇.ey[i,j]
 
-# Physics
-x          = (min=-3.0, max=3.0)  
-y          = (min=-5.0, max=0.0)
-ε̇bg        = -1.0
-rad        = 0.5
-y0         = -2.
-g          = -1
-inclusion  = true
-adapt_mesh = true
-solve      = true
-# Numerics
-nc         = (x=21,     y=21    )  # numerical grid resolution
-nv         = (x=nc.x+1, y=nc.y+1)  # numerical grid resolution
-ε          = 1e-8          # nonlinear tolerance
-iterMax    = 20            # max number of iters
-# Preprocessing
-Δ          = (x=(x.max-x.min)/nc.x, y=(y.max-y.min)/nc.y)
-# Array initialisation
-V        = ( x   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)),  # v: vertices --- c: centroids
-             y   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)) ) 
-ε̇        = ( xx  = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),  # ex: x edges --- ey: y edges
-             yy  = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             xy  = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)) )
-τ        = ( xx  = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),  # ex: x edges --- ey: y edges
-             yy  = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             xy  = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)) )
-∇v̇       = (        ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)  )
-P        = (        ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)  )
-D        = ( v11 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             v12 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),  
-             v13 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             v21 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             v22 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             v23 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             v31 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             v32 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)),
-             v33 = (ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)), )
-∂ξ       = ( ∂x  = (v  =  ones(nv.x,   nv.y), c  =  ones(nc.x+2, nc.y+2), ey =  ones(nc.x+2, nv.y), ex =  ones(nv.x,   nc.y+2)), 
-             ∂y  = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2), ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)) )
-∂η       = ( ∂x  = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2), ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)), 
-             ∂y  = (v  =  ones(nv.x,   nv.y), c  =  ones(nc.x+2, nc.y+2), ey =  ones(nc.x+2, nv.y), ex =  ones(nv.x,   nc.y+2)) )
-             
-R        = ( x   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)), 
-             y   = (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)) ) 
-ρ        = ( (v  = zeros(nv.x,   nv.y), c  = zeros(nc.x+2, nc.y+2)) )
-η        = (        ey = zeros(nc.x+2, nv.y), ex = zeros(nv.x,   nc.y+2)  )
-# Fine mesh
-xxv, yyv    = LinRange(x.min-Δ.x/2, x.max+Δ.x/2, 2nc.x+3), LinRange(y.min-Δ.y/2, y.max+Δ.y/2, 2nc.y+3)
-(xv4,yv4) = ([x for x=xxv,y=yyv], [y for x=xxv,y=yyv])
-xv2_1, yv2_1 = xv4[2:2:end-1,2:2:end-1  ], yv4[2:2:end-1,2:2:end-1  ]
-xv2_2, yv2_2 = xv4[1:2:end-0,1:2:end-0  ], yv4[1:2:end-0,1:2:end-0  ]
-xc2_1, yc2_1 = xv4[3:2:end-2,2:2:end-1  ], yv4[3:2:end-2,2:2:end-1  ]
-xc2_2, yc2_2 = xv4[2:2:end-1,3:2:end-2+2], yv4[2:2:end-1,3:2:end-2+2]
-x = merge(x, (v=xv4[2:2:end-1,2:2:end-1  ], c=xv4[1:2:end-0,1:2:end-0  ], ey=xv4[1:2:end,2:2:end-1  ], ex=xv4[2:2:end-1,1:2:end]) ) 
-y = merge(x, (v=yv4[2:2:end-1,2:2:end-1  ], c=yv4[1:2:end-0,1:2:end-0  ], ey=yv4[1:2:end,2:2:end-1  ], ex=yv4[2:2:end-1,1:2:end]) ) 
-# Velocity
-V.x.v .= -ε̇bg.*x.v; V.x.c .= -ε̇bg.*x.c
-V.y.v .=  ε̇bg.*y.v; V.y.c .=  ε̇bg.*y.c
-# Viscosity
-η.ey  .= 1.0; η.ex  .= 1.0
-if inclusion
-    η.ey[x.ey.^2 .+ (y.ey.-y0).^2 .< rad] .= 100.
-    η.ex[x.ex.^2 .+ (y.ex.-y0).^2 .< rad] .= 100.
+        end
+    end
 end
-D.v11.ey .= 2 .* η.ey; D.v11.ex .= 2 .* η.ex
-D.v22.ey .= 2 .* η.ey; D.v22.ex .= 2 .* η.ex
-D.v33.ey .= 2 .* η.ey; D.v33.ex .= 2 .* η.ex
-# Density
-ρ.v  .= 1.0; ρ.c  .= 1.0
-if inclusion
-    ρ.v[x.v.^2 .+ (y.v.-y0).^2 .< rad] .= 2.
-    ρ.c[x.c.^2 .+ (y.c.-y0)^2 .< rad] .= 2.
-end
-# Residual
-∇v̇.ey[2:end-1,:]   .=  ∂_∂x(V.x.v,V.x.c[2:end-1,:], Δ.x, Δ.y, ∂ξ.∂x.ey[2:end-1,:], ∂η.∂x.ey[2:end-1,:] ) .+ ∂_∂y(V.y.c[2:end-1,:],V.y.v, Δ.x, Δ.y, ∂ξ.∂y.ey[2:end-1,:], ∂η.∂y.ey[2:end-1,:] ) 
-∇v̇.ex[:,2:end-1]   .=  ∂_∂x(V.x.c[:,2:end-1],V.x.v, Δ.x, Δ.y, ∂ξ.∂x.ex[:,2:end-1], ∂η.∂x.ex[:,2:end-1] ) .+ ∂_∂y(V.y.v,V.y.c[:,2:end-1], Δ.x, Δ.y, ∂ξ.∂y.ex[:,2:end-1], ∂η.∂y.ex[:,2:end-1] ) 
-ε̇.xx.ey[2:end-1,:] .=  ∂_∂x(V.x.v,V.x.c[2:end-1,:], Δ.x, Δ.y, ∂ξ.∂x.ey[2:end-1,:], ∂η.∂x.ey[2:end-1,:] ) .- ∇v̇.ey[2:end-1,:]
-ε̇.yy.ey[2:end-1,:] .=  ∂_∂y(V.y.c[2:end-1,:],V.y.v, Δ.x, Δ.y, ∂ξ.∂y.ey[2:end-1,:], ∂η.∂y.ey[2:end-1,:] ) .- ∇v̇.ey[2:end-1,:]
-ε̇.xx.ex[:,2:end-1] .=  ∂_∂x(V.x.c[:,2:end-1],V.x.v, Δ.x, Δ.y, ∂ξ.∂x.ex[:,2:end-1], ∂η.∂x.ex[:,2:end-1] ) .- ∇v̇.ex[:,2:end-1]
-ε̇.yy.ex[:,2:end-1] .=  ∂_∂y(V.y.v,V.y.c[:,2:end-1], Δ.x, Δ.y, ∂ξ.∂y.ex[:,2:end-1], ∂η.∂y.ex[:,2:end-1] ) .- ∇v̇.ex[:,2:end-1]
-ε̇.xy.ey[2:end-1,:] .= (∂_∂y(V.x.c[2:end-1,:],V.x.v, Δ.x, Δ.y, ∂ξ.∂y.ey[2:end-1,:], ∂η.∂y.ey[2:end-1,:]) .+ ∂_∂x(V.y.v,V.y.c[2:end-1,:], Δ.x, Δ.y, ∂ξ.∂x.ey[2:end-1,:], ∂η.∂x.ey[2:end-1,:]) ) / 2.
-ε̇.xy.ex[:,2:end-1] .= (∂_∂y(V.x.v,V.x.c[:,2:end-1], Δ.x, Δ.y, ∂ξ.∂y.ex[:,2:end-1], ∂η.∂y.ex[:,2:end-1]) .+ ∂_∂x(V.y.c[:,2:end-1],V.y.v, Δ.x, Δ.y, ∂ξ.∂x.ex[:,2:end-1], ∂η.∂x.ex[:,2:end-1]) ) / 2.
-# τxx_1 .= 2.0 .* η_1 .* ε̇xx_1
-# τxx_2 .= 2.0 .* η_2 .* ε̇xx_2
-# τyy_1 .= 2.0 .* η_1 .* ε̇yy_1
-# τyy_2 .= 2.0 .* η_2 .* ε̇yy_2
-# τxy_1 .= 2.0 .* η_1 .* ε̇xy_1
-# τxy_2 .= 2.0 .* η_2 .* ε̇xy_2
-# Rx_1[2:end-1,2:end-0] .= ∂_∂x(τxx_1[:,2:end-0],τxx_2[2:end-1,:],Δx,Δy,∂ξ∂xv_1[2:end-1,2:end-0],∂η∂xv_1[2:end-1,2:end-0]) .+ ∂_∂y(τxy_2[2:end-1,:],τxy_1[:,2:end-0],Δx,Δy,∂ξ∂yv_1[2:end-1,2:end-0],∂η∂yv_1[2:end-1,2:end-0]) .-  ∂_∂x(P_1[:,2:end-0],P_2[2:end-1,:],Δx,Δy,∂ξ∂xv_1[2:end-1,2:end-0],∂η∂xv_1[2:end-1,2:end-0])
-# Rx_2                  .= ∂_∂x(τxx_2[:,1:end-1],τxx_1,           Δx,Δy,∂ξ∂xv_2[2:end-1,2:end-1],∂η∂xv_2[2:end-1,2:end-1]) .+ ∂_∂y(τxy_1,τxy_2[:,1:end-1],           Δx,Δy,∂ξ∂yv_2[2:end-1,2:end-1],∂η∂yv_2[2:end-1,2:end-1]) .-  ∂_∂x(P_2[:,1:end-1],P_1,           Δx,Δy,∂ξ∂xv_2[2:end-1,2:end-1],∂η∂xv_2[2:end-1,2:end-1]) 
-# Ry_1[2:end-1,2:end-0] .= ∂_∂y(τyy_2[2:end-1,:],τyy_1[:,2:end-0],Δx,Δy,∂ξ∂yv_1[2:end-1,2:end-0],∂η∂yv_1[2:end-1,2:end-0]) .+ ∂_∂x(τxy_1[:,2:end-0],τxy_2[2:end-1,:],Δx,Δy,∂ξ∂xv_1[2:end-1,2:end-0],∂η∂xv_1[2:end-1,2:end-0]) .-  ∂_∂y(P_2[2:end-1,:],P_1[:,2:end-0],Δx,Δy,∂ξ∂yv_1[2:end-1,2:end-0],∂η∂yv_1[2:end-1,2:end-0]) .+ ρ_1[2:end-1,2:end-0].*g
-# Ry_2                  .= ∂_∂y(τyy_1,τyy_2[:,1:end-1],           Δx,Δy,∂ξ∂yv_2[2:end-1,2:end-1],∂η∂yv_2[2:end-1,2:end-1]) .+ ∂_∂x(τxy_2[:,1:end-1],τxy_1,           Δx,Δy,∂ξ∂xv_2[2:end-1,2:end-1],∂η∂xv_2[2:end-1,2:end-1]) .-  ∂_∂y(P_1,P_2[:,1:end-1],           Δx,Δy,∂ξ∂yv_2[2:end-1,2:end-1],∂η∂yv_2[2:end-1,2:end-1]) .+ ρ_2.*g
-# Rp_1                  .= .-∇v_1
-# Rp_2                  .= .-∇v_2[:,1:end-1]
+
+Main_2D_DI()
